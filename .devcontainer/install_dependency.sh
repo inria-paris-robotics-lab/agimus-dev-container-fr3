9cede1ec7c2c7e34e60fd4b20e1ec8effb71aed8
Florent Lamiraux <florent@laas.fr>
2024-11-27 10:38:09 +0100

    Downgrade pinocchio to 3.0.0.
      - Use gepetto-viewer updated for ROS 2,
      - install hpp_benchmark.

--- .devcontainer/install_dependency.sh
+++ .devcontainer/install_dependency.sh
@@ -11,20 +11,6 @@ TAG="${split[2]}"
 CORES="${split[3]}"
 
 git clone --branch "$TAG" --recursive "https://github.com/$ORG/$PRJ"
-cmake -B "$PRJ/build" -S "$PRJ" \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    -DBUILD_TESTING=OFF \
-    -DBUILD_BENCHMARK=OFF \
-    -DBUILD_BENCHMARKS=OFF \
-    -DBUILD_EXAMPLES=OFF \
-    -DINSTALL_DOCUMENTATION=OFF \
-    -DBUILD_PYTHON_INTERFACE=ON \
-    -DGENERATE_PYTHON_STUBS=OFF \
-    -DCOAL_BACKWARD_COMPATIBILITY_WITH_HPP_FCL=ON \
-    -DCOAL_HAS_QHULL=ON \
-    -DBUILD_WITH_COLLISION_SUPPORT=ON \
-    -DPROJECT_USE_QT4=OFF \
-    -DUSE_QPOASES=OFF \
-    -Wno-dev
+cmake -B "$PRJ/build" -S "$PRJ" ${CMAKE_FLAGS} -Wno-dev
 cmake --build "$PRJ/build" -j $CORES
 sudo cmake --build "$PRJ/build" -t install
